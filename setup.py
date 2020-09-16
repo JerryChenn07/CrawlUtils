fb5f7ff7e57d952c514a9f1701f8b8249734b260
CJR妙传C罗戏耍西西 <32920461+cjr0707@users.noreply.github.com>
2020-09-16 16:32:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # https://github.com/kingname/GeneralNewsExtractor/blob/master/setup.py
 setup(
     name='crawl_utils',
-    version='0.1.0',
+    version='0.1.2',
     description='Commonly Used Crawl Utils',
     author='cjr',
     author_email='cjr0707@qq.com',
