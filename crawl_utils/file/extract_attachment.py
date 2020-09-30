40b1b3fda24e2f1bb65dd39e007866fd525e067e
CJR on MBP <cjr0707@qq.com>
2020-09-30 16:53:33 +0800

    :sparkles: duplicate filter & get attach name from text&title

--- crawl_utils/file/extract_attachment.py
+++ crawl_utils/file/extract_attachment.py
@@ -20,24 +20,31 @@ def extract_attachment(text, content_url, attachment_format_list=[]):
     if not isinstance(text, str):
         raise Exception('new version has removed response obj, please change codes or upgrade')
     attachment_list = []
-    base_attachment_format = 'pdf|xls|doc|ppt|wps'
-    if attachment_format_list:
-        base_attachment_format = base_attachment_format + '|' + '|'.join(attachment_format_list)
-    attachment_format_patten = re.compile(f'\.({base_attachment_format})[a-z]?$', flags=re.IGNORECASE)
+
+    base_attachment_format_list = ['pdf', 'xls', 'doc', 'ppt', 'wps', 'txt']
+    attachment_format = '|'.join(set(base_attachment_format_list + attachment_format_list))  # 'pdf|xls|doc|ppt|wps|txt'
+    attachment_format_patten = re.compile(f'\.({attachment_format})[a-z]?$', flags=re.IGNORECASE)
 
     get_node_a_list = re.findall('<a .*?</a>', text, re.DOTALL | re.IGNORECASE)
     suspect_attachment_list = Selector(''.join(get_node_a_list)).xpath('//a')
+    attachment_set = set()
     for s in suspect_attachment_list:
         if not s.xpath('./@href').re_first(attachment_format_patten):
             continue
-        origin_file_name = s.xpath('string()').get('').strip()
-        if not origin_file_name:
-            logger.warning(f"Get a empty attachment name, origin node is ==={s.get()}===, content_url={content_url}")
+        origin_file_name_from_text = s.xpath('string()').get('').strip()
+        origin_file_name_from_title = s.xpath('./@title').get('')
+        if not origin_file_name_from_text and not origin_file_name_from_title:
             # continue # 存在一些附件名为空的情况
+            logger.warning(f"Get a empty attachment name, origin node is ==={s.get()}===, content_url={content_url}")
 
+        origin_file_name = origin_file_name_from_text or origin_file_name_from_title
         attachment_name = attachment_format_patten.sub('', origin_file_name)
-        part_attachment_url = s.xpath('./@href').get()
-        file_info = {'attachment_name': attachment_name, 'attachment_url': urljoin(content_url, part_attachment_url)}
+        attachment_url = urljoin(content_url, s.xpath('./@href').get())
+        if attachment_name + attachment_url in attachment_set:
+            continue
+
+        attachment_set.add(attachment_name + attachment_url)
+        file_info = {'attachment_name': attachment_name, 'attachment_url': attachment_url}
         logger.debug(f"Get attachment successful: {file_info}")
         attachment_list.append(file_info)
 
